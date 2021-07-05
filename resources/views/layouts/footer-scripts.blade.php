62c6d3e68069f68ae8044bc34e1ac5407700c862
marwanhaz43 <marwanhaz43@gmail.com>
2021-07-05 14:12:06 -0700

    first commit

---
+++ resources/views/layouts/footer-scripts.blade.php
@@ -0,0 +1,14 @@
+    <!-- footer  style-->
+    <script   src="{{ URL::asset('assets/js/jquery-2.2.4.min.js') }}"></script>
+    <script   src="{{ URL::asset('assets/js/bootstrap.min.js') }}"></script>
+    <script type="text/javascript"   src="{{ URL::asset('assets/js/jquery.bxslider.js') }}"></script>
+    <!-- owl carousel-->
+    <script   src="{{ URL::asset('assets/CSS/owlcarousel/owl.carousel.min.js') }}"></script>
+    <script   src="{{ URL::asset('assets/js/jquery.easing.min.js') }}"></script>
+    <script   src="{{ URL::asset('assets/js/jquery.mCustomScrollbar.concat.min.js') }}"></script>
+    <script   src="{{ URL::asset('assets/js/zebra_datepicker.js') }}"></script>
+    <!-- jQuery ui js-->
+    <script   src="{{ URL::asset('assets/js/jquery.mixitup.min.js') }}"></script>
+    <script   src="{{ URL::asset('assets/js/jquery.fancybox.pack.js') }}"></script>
+    <script   src="{{ URL::asset('assets/CSS/jquery-ui-1.11.4/jquery-ui.min.js') }}"></script>
+    <script   src="{{ URL::asset('assets/js/custom.js') }}"></script>
---
+++ resources/views/layouts/head-script.blade.php
@@ -0,0 +1,29 @@
+<meta charset="UTF-8">
+<title>Lake Resort || Responsive BootStrap HTML 5 Template</title>
+<!-- reponsive meta-->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Bootstrap-->
+<link  href="{{URL::asset('assets/css/bootstrap.min.css')}}" rel="stylesheet">
+<link href="{{URL::asset('assets/css/font-awesome.min.css')}}"   rel="stylesheet">
+<!-- strock gap icons-->
+<link rel="stylesheet" href="{{URL::asset('assets/css/Stroke-Gap-Icons-Webfont/style.css')}}"  >
+<link rel="stylesheet" href="{{URL::asset('assets/css/animate.min.css')}}"  >
+<!-- owl-carousel-->
+<link rel="stylesheet"  href="{{URL::asset('assets/css/owlcarousel/owl.carousel.css')}}"    >
+<link rel="stylesheet" href="{{URL::asset('assets/css/imagelightbox/imagelightbox.min.css')}}"  >
+<link rel="stylesheet"  href="{{URL::asset('assets/css/jquery-ui-1.11.4/jquery-ui.min.css')}}"  >
+<!-- Main Css-->
+<link rel="stylesheet"  href="{{URL::asset('assets/css/style.css')}}"  >
+<link rel="stylesheet"href="{{URL::asset('assets/css/responsive.css')}}"  >
+<link rel="icon" type="image/png" sizes="16x16" href="{{URL::asset('assets/img/favicon-16x16.png')}}"  >
+<!--if IE-->
+<link rel="stylesheet" type="text/css" href="{{URL::asset('assets/css/all-ie-only.css')}}"  >
+
+@yield('css')
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries-->
+<!-- WARNING: Respond.js doesn't work if you view the page via file://-->
+<!--if lt IE 9
+script(src='https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js')
+script(src='https://oss.maxcdn.com/respond/1.4.2/respond.min.js')
+
+-->
